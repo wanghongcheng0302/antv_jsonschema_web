0f28899cf05f13ca1a7cb17cf0e623a49c343842
whc <249768447@qq.com>
2022-02-23 02:14:46 +0800

    :tada:初始化

---
+++ config/routes.ts
@@ -0,0 +1,75 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/antv/examples',
+    name: 'antv-examples',
+    access: 'canAdmin',
+    component: './G2Examples',
+    icon: 'bars',
+    routes: [
+      { 
+        path: '/antv/examples/g2',
+        name: 'antv-examples-g2',
+        component: './G2Examples',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  // {
+  //   path: '/admin',
+  //   name: 'admin',
+  //   icon: 'crown',
+  //   access: 'canAdmin',
+  //   component: './Admin',
+  //   routes: [
+  //     {
+  //       path: '/admin/sub-page',
+  //       name: 'sub-page',
+  //       icon: 'smile',
+  //       component: './Welcome',
+  //     },
+  //     {
+  //       component: './404',
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: 'list.table-list',
+  //   icon: 'table',
+  //   path: '/list',
+  //   component: './TableList',
+  // },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
---
+++ src/pages/G2Examples/schemas/index.ts
@@ -0,0 +1,60 @@
+import violin from "./violin.json";
+import radar from "./radar.json";
+import treemap from "./treemap.json";
+import pie from "./pie.json";
+import liquid from "./liquid.json";
+import venn from "./venn.json";
+import gauge from "./gauge.json";
+import progress from "./progress.json";
+import waterfall from "./waterfall.json";
+import funnel from "./funnel.json";
+import circlePacking from "./circle-packing.json";
+import bar from "./bar.json";
+import dualAxes from "./dual-axes.json";
+import sunburst from "./sunburst.json";
+import bullet from "./bullet.json";
+import wordCloud from "./word-cloud.json";
+import area from "./area.json";
+import column from "./column.json";
+import bidirectionalBar from "./bidirectional-bar.json";
+import ringProgress from "./ring-progress.json";
+import stock from "./stock.json";
+import box from "./box.json";
+import histogram from "./histogram.json";
+import tinyArea from "./tiny-area.json";
+import radialBar from "./radial-bar.json";
+import tinyColumn from "./tiny-column.json";
+import rose from "./rose.json";
+import line from "./line.json";
+
+
+export default {
+    violin,
+    radar,
+    treemap,
+    pie,
+    liquid,
+    venn,
+    gauge,
+    progress,
+    waterfall,
+    funnel,
+    circlePacking,
+    bar,
+    dualAxes,
+    sunburst,
+    bullet,
+    wordCloud,
+    area,
+    column,
+    bidirectionalBar,
+    ringProgress,
+    stock,
+    box,
+    histogram,
+    tinyArea,
+    radialBar,
+    tinyColumn,
+    rose,
+    line,
+}
